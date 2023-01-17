d86053737db362019a860dda1e07a605aaea8267
denis-meshcheryakov <ktulhoo.pyneng@yandex.ru>
2023-01-17 23:46:05 +0300

    Более сложные запросы в DB

--- queries.py
+++ queries.py
@@ -1,3 +1,7 @@
+from sqlalchemy import desc
+from sqlalchemy.sql import func
+
+from db import db_session
 from model_2 import Salary
 
 def top_salary(num_rows):
@@ -17,7 +21,28 @@ def top_salary_by_domain(domain, num_rows):
     for s in top_salary:
         print(f'З/п: {s.salary}')
 
+def average_salary():
+    avg_salary = db_session.query(func.avg(Salary.salary)).scalar()
+    print(f'Средняя зарплата {avg_salary:.2f}')
+
+def count_distinct_cities():
+    count_cities = db_session.query(Salary.city).group_by(Salary.city).count()
+    print(f'Кол-во городов {count_cities}')
+
+def top_avg_salary_by_city(num_rows):
+    top_salary = db_session.query(
+        Salary.city,
+        func.avg(Salary.salary).label('avg_salary')
+    ).group_by(Salary.city).order_by(desc('avg_salary')).limit(num_rows)
+
+    for city, salary in top_salary:
+        print(f'Город {city} - з/п {salary:.2f}')
+
+
 if __name__ =="__main__":
     # top_salary(5)
     # salary_by_city('Кимры')
-    top_salary_by_domain('@yandex.ru', 5)
+    # top_salary_by_domain('@yandex.ru', 5)
+    # average_salary()
+    # count_distinct_cities()
+    top_avg_salary_by_city(5)
