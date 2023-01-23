c19dd92c50d3b911eec58be1ce937ac340b795e7
denis-meshcheryakov <ktulhoo.pyneng@yandex.ru>
2023-01-24 01:43:12 +0300

    Профилировка SQL-запросов

--- DB_4_migration/queries.py
+++ DB_4_migration/queries.py
@@ -1,9 +1,6 @@
-import time
 from models import Employee
 
 
 if __name__ == "__main__":
-    start = time.perf_counter()
-    for _ in range(10):
-        Employee.query.filter(Employee.job == 'Геолог').all()
-    print(f'{time.perf_counter() - start}')
+    employee = Employee.query.first()
+    print(employee.company.name)
